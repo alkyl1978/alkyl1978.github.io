a78d37eeec247783ff42cfc9289582378345cacb
алексей <acn@hp>
2021-01-05 02:24:42 +0300

    prod nginx

--- bin/run.sh
+++ bin/run.sh
@@ -4,6 +4,3 @@ set -e
 npm install
 npm run build
 rm -rf node_modules scaffolds source themes package.json package-lock.json _config.yml db.json
-cp bin/package.json package.json
-npm install serve
-ls -a
