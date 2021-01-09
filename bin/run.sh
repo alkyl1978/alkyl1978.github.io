a09d5360044b4379a432e9a7ad760cb197dadca1
алексей <acn@hp>
2021-01-09 14:28:15 +0300

    test version

--- bin/run.sh
+++ bin/run.sh
@@ -3,4 +3,4 @@ set -x
 set -e
 npm install
 npm run build
-rm -rf node_modules scaffolds source themes package.json package-lock.json _config.yml db.json
+#rm -rf node_modules scaffolds source themes package.json package-lock.json _config.yml db.json
