8b0517425299b500e1881065bc2bf4ca30faddbc
aleks <aleks@MONOBLOK2.lan>
2023-01-14 23:56:11 +0300

    update

--- bin/run.sh
+++ bin/run.sh
@@ -3,6 +3,7 @@ set -x
 set -e
 mkdir /root/.ssh/
 echo $DEPLOY_KEY > /root/.ssh/id_rsa
+chmod 600 /root/.ssh/id_rsa
 ssh-keyscan -t rsa github.com >> /root/.ssh/known_hosts
 npm install --production
 npm run build
